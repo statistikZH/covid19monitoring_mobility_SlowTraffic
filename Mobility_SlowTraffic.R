27c63fa77955abe23e2714a68192c0698d010632
Peter Moser <peterjamoser@bluewin.ch>
2020-03-30 15:11:27 +0200

    removed oev hardbruecke

--- Mobility_SlowTraffic.R
+++ Mobility_SlowTraffic.R
@@ -8,43 +8,6 @@ library (lattice)
 library(chron)
 library(reshape)
 
-################################
-# Preliminary Code subject to review, Results to be viewed with caution!! 
-# Download data
-urlfile="https://data.stadt-zuerich.ch/dataset/55c68924-bb53-40a4-8f62-69e063cb2afe/resource/0132f2f4-3d62-4765-bcd8-05994add7127/download/frequenzen_hardbruecke_2020.csv"
-zhoev<-data.frame(read_csv(url(urlfile)))
-################################
-# 
-zhoev$date<-date(as.POSIXct(zhoev$Timestamp))
-# ohne aktuellen Tag
-zhoev<-subset(zhoev, date!=Sys.Date())
-#Aggregate der Zähllinien pro tag (date) In und Out Zusammengezählt
-oevtot<-with(zhoev, tapply(In+Out, list(date, Name), sum))
-
-hardoev<-data.frame(date=as.POSIXct(paste(rownames(oevtot), "00:00:00", sep=" ")),
-                       value=round(apply(oevtot[,c("Ost-Nord total",
-                                             "Ost-SBB total", 
-                                             "Ost-Süd total",
-                                             "West-Nord total", 
-                                             "West-SBB total", 
-                                             "Ost-Süd total")], 1, sum)/1000, 2),
-                       topic="Mobilität",
-                       variable_short="oev_freq_hardbruecke",
-                       variable_long="ÖV-Besucherfrequenzen, Zählstelle Hardbrücke",
-                       location="Stadt Zürich",
-                       unit="Anzahl in 1000",
-                       source="VBZ",
-                       update="täglich",
-                       public="ja",
-                       description="https://github.com/statistikZH/covid19monitoring_mobility_SlowTraffic")
-
-
-#write the final file for publication
-write.table(hardoev, "Mobility_SlowTraffic.csv", sep=",", fileEncoding="UTF-8", row.names = F)
-
-
-
-
 
 
 
@@ -185,6 +148,15 @@ fussfreq<-data.frame(
 allfreq<-rbind(velofreq, fussfreq)
 
 ################################
+
+
+
+#write the final file for publication
+write.table(hardoev, "Mobility_SlowTraffic.csv", sep=",", fileEncoding="UTF-8", row.names = F)
+
+
+
+
 
 # export result
 write.table(allfreq, "./velo_fuss_freq_261.csv", sep=",", fileEncoding="UTF-8", row.names = F)
