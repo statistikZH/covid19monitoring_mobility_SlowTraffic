4b496b2e96cc33209f4b11da6940304632dc08a1
Peter Moser <peterjamoser@bluewin.ch>
2020-03-30 14:19:09 +0200

    Oev Zählstelle Hardbrücke

--- Mobility_SlowTraffic.R
+++ Mobility_SlowTraffic.R
@@ -1,7 +1,4 @@
-# Mobility_SlowTraffic.R
-#https://data.stadt-zuerich.ch/dataset/ted_taz_verkehrszaehlungen_werte_fussgaenger_velo
-#https://data.stadt-zuerich.ch/dataset/geo_standorte_der_automatischen_fuss__und_velozaehlungen
-
+# Mobility_SlowTraffic_OEV.R
 # Import libraries
 require(tidyquant)
 require(xts)
@@ -10,6 +7,84 @@ library (readr)
 library (lattice)
 library(chron)
 library(reshape)
+
+################################
+# Preliminary Code subject to review, Results to be viewed with caution!! 
+# Download data
+urlfile="https://data.stadt-zuerich.ch/dataset/55c68924-bb53-40a4-8f62-69e063cb2afe/resource/0132f2f4-3d62-4765-bcd8-05994add7127/download/frequenzen_hardbruecke_2020.csv"
+zhoev<-data.frame(read_csv(url(urlfile)))
+
+
+
+################################
+# Format data according to data structure specification
+#Zählstellen, Zahl der Messungen
+zhoev$date<-date(as.POSIXct(zhoev$Timestamp))
+zhoev<-subset(zhoev, date!=Sys.Date())
+
+
+oevtot<-with(zhoev, tapply(In+Out, list(date, Name), sum))
+
+
+
+
+
+
+
+as#Tagesfrequenzen Velo beide Richtungen
+velodaily<-with(zhlv, tapply(VELO_OUT+VELO_IN, list(datum, FK_STANDORT), sum, na.rm=T))
+#Velozählstellen an denen 2020 überhaupt etwas gemessen wird
+zst<-apply(velodaily,2, sum, na.rm=T)>0
+names(zst[zst==T])
+zst<-c("52", "53", "60", "732", "1037", "1692", "2976", "2978", "2979", 
+       "2980", "2986", "2989", "2991", "2993", "2996", "2997", "3277", 
+       "3598", "3923", "3924", "3926", "3927")
+velodaily<-velodaily[,zst]
+velotot<-data.frame(velofreq=apply(velodaily,1, sum, na.rm=T))
+
+velofreq<-data.frame(
+  date=as.POSIXct(paste(rownames(velotot), "00:00:00", sep=" ")), 
+  value=velotot$velofreq, 
+  topic="mobilität", 
+  variable="velo_tot_261",
+  location="Stadt Zürich",
+  unit="Anzahl Velos", 
+  origin="Stadt Zürich",
+  update="daily",
+  public="ja",
+  description="kumulierte tägliche Anzahl Velos an allen Velo-zählstellen der Stadt Zürich")
+
+#Tagesfrequenzen FUSS beide Richtungen
+fussdaily<-with(zhlv, tapply(FUSS_OUT+FUSS_IN, list(datum, FK_STANDORT), sum, na.rm=T))
+#Fussverkehrszählstellen an denen 2020 überhaupt etwas gemessen wird
+#zst<-apply(fussdaily,2, sum, na.rm=T)>0
+#edit(names(zst)[zst==T]) 
+zst<-c("2", "20", "39", "394", "1357", "1358", #? warum sind folgende Werte nicht dabei?: 32, 52, 53, 65, 2994
+       "1681", "2640", "2641", "2642", "2961", "2984", "2985", 
+       "3279")
+fussdaily<-fussdaily[,zst]
+fusstot<-data.frame(fussfreq=apply(fussdaily,1, sum, na.rm=T))
+
+fussfreq<-data.frame(
+  date=as.POSIXct(paste(rownames(fusstot), "00:00:00", sep=" ")), 
+  value=fusstot$fussfreq, 
+  topic="mobilität", 
+  variable="fuss_tot_261",
+  location="Stadt Zürich",
+  unit="Anzahl Fussgänger", 
+  origin="Stadt Zürich",
+  update="daily",
+  public="ja",
+  description="kumulierte tägliche Anzahl Passanten (beide Richtungen) an allen aktiven Fussverkehrzählstellen der Stadt Zürich")
+
+
+allfreq<-rbind(velofreq, fussfreq)
+
+
+
+
+
+
 
 ################################
 # Preliminary Code subject to review, Results to be viewed with caution!! 
